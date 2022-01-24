1395f09467e176ae589b65d401d437bab8c84bd1
davidracovan <74030080+superpenguin612@users.noreply.github.com>
2022-01-24 17:16:19 -0500

    Update Camera.java with limelight code

--- 2022-robot/src/main/java/frc/robot/auton/Camera.java
+++ 2022-robot/src/main/java/frc/robot/auton/Camera.java
@@ -1,5 +1,31 @@
 package frc.robot.auton;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 public class Camera {
-    
+    private static NetworkTable table;
+
+    public Camera() {
+        table = NetworkTableInstance.getDefault().getTable("limelight");
+    }
+
+    public double getTx() {
+        NetworkTableEntry tx = table.getEntry("tx");
+        return tx.getDouble(0.0);
+    }
+    public double getTy() {
+        NetworkTableEntry ty = table.getEntry("ty");
+        return ty.getDouble(0.0);
+    }
+    public double getTv() {
+        NetworkTableEntry tv = table.getEntry("tv");
+        return tv.getDouble(0.0);
+    }
+    public double getTa() {
+        NetworkTableEntry ta = table.getEntry("ta");
+        return ta.getDouble(0.0);
+    }
+
 }
