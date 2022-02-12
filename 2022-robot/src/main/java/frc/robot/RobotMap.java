4dcf88b289b8e22837126d4a3a93a0c38feccbde
davidracovan <74030080+superpenguin612@users.noreply.github.com>
2022-02-12 13:12:22 -0500

    Swap shooter primary and climber primary IDs

--- 2022-robot/src/main/java/frc/robot/RobotMap.java
+++ 2022-robot/src/main/java/frc/robot/RobotMap.java
@@ -33,7 +33,7 @@ public class RobotMap {
         public static final int UPDOWNER22 = 4;
     }
     public static class Climber {
-        public static final int C_PRIMARY = 8;
+        public static final int C_PRIMARY = 12;
         public static final int C_SECONDARY = 9;
         public static final int UPDOWNER11 = 5;
         public static final int UPDOWNER12 = 6;
@@ -44,7 +44,7 @@ public class RobotMap {
         public static final boolean IS_INVERTED = false;
     }
     public static class Shooter {
-        public static final int S_PRIMARY = 12;
+        public static final int S_PRIMARY = 8;
         public static final int S_SECONDARY = 13;
         public static final boolean IS_INVERTED = false;
     }
