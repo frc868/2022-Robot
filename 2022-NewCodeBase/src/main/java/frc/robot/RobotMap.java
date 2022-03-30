9df31b41f20313011c9536e6489a4a2939a7f15c
davidracovan <74030080+superpenguin612@users.noreply.github.com>
2022-03-30 19:43:47 -0400

    Add I to limelight PID to prevent SSE (found at tippe)

--- 2022-NewCodeBase/src/main/java/frc/robot/RobotMap.java
+++ 2022-NewCodeBase/src/main/java/frc/robot/RobotMap.java
@@ -163,7 +163,7 @@ public class RobotMap {
              */
             public static class TurnToLimelight {
                 public static final double KP = 0.022;
-                public static final double KI = 0.00;
+                public static final double KI = 0.005;
                 public static final double KD = 0.0006;
             }
 
@@ -190,9 +190,9 @@ public class RobotMap {
              * Notes: This will be re-tuned this week.
              */
             public static class TurnToAstra {
-                public static final double KP = 0.0022;
-                public static final double KI = 0;
-                public static final double KD = 0;
+                public static final double KP = 0.020;
+                public static final double KI = 0.0105;
+                public static final double KD = 0.0045;
             }
 
             /**
