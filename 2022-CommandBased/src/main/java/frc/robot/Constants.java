465fe01c0ed79e8bcf87c0b6064fe5aaf6e6ec46
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2022-04-24 10:49:08 -0400

    Testing github.dev for future years!

--- 2022-CommandBased/src/main/java/frc/robot/Constants.java
+++ 2022-CommandBased/src/main/java/frc/robot/Constants.java
@@ -14,7 +14,7 @@ package frc.robot;
  * <p>
  * It is advised to statically import this class (or one of its inner classes)
  * wherever the
- * constants are needed, to reduce verbosity.
+ * constants are needed, to reduce verbosity. 
  */
 public final class Constants {
     public static final class Drivetrain {
